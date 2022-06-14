b48e9d50b5419c73490563746cecbcd0b72ecdf3
Jack Meng <jackmeng0814@gmail.com>
2022-06-14 16:06:46 +0000

    `rm` removed deprecated classes that would not be needed here

--- src/com/jackmeng/app/connections/properties/PropertiesManager.java
+++ src/com/jackmeng/app/connections/properties/PropertiesManager.java
@@ -3,7 +3,6 @@ package com.jackmeng.app.connections.properties;
 import java.util.Properties;
 import java.util.Map;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
