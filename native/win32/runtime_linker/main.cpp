479f01c30be5a3d6c3cc1c6d53dffb4e820c996f
Jack Meng <jackmeng0814@gmail.com>
2022-06-15 22:15:50 -0400

    `add-header`

--- native/win32/runtime_linker/main.cpp
+++ native/win32/runtime_linker/main.cpp
@@ -1,15 +1,30 @@
+/*
+ *  Copyright: (C) 2022 name of Jack Meng
+ * Halcyon MP4J is music-playing software.
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program; If not, see <http://www.gnu.org/licenses/>.
+ */
+
 /**
- @file main.cpp
- @author Jack Meng
- @brief This represents the compiled binary that is attached to run the JAR
- file on Win32 distributions of this program.
- @version 0.1
- @date 2022-06-15
+ * @file main.cpp
+ * @author Jack Meng
+ * @brief This represents the compiled binary that is attached to run the JAR
+ * file on Win32 distributions of this program.
+ * @version 0.1
+ * @date 2022-06-15
  *
- @copyright Copyright (c) 2022
+ * @copyright Copyright (c) 2022
  *
- The generated binary from this native implementation should be attached both
- for a x32 and x64 version of the program.
+ * The generated binary from this native implementation should be attached both
+ * for a x32 and x64 version of the program.
  */
 
 #include "win32.hpp"
