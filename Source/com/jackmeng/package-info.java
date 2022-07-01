e5a37175a897732c7f7c3bbee4b38601172b8b60
Jack Meng <jackmeng0814@gmail.com>
2022-07-01 16:07:38 -0400

    `format`

--- Source/com/jackmeng/package-info.java
+++ Source/com/jackmeng/package-info.java
@@ -2,7 +2,8 @@
  * This package contains 3 main packages for each individual functions:
  * Cosmos -> Anything related to GUI or anything Graphical
  * Halcyon -> Anything related to the program itself that is not audio or GUI
- * Tailwind -> The Audio Framework used to play audio and interface with audio playback.
+ * Tailwind -> The Audio Framework used to play audio and interface with audio
+ * playback.
  *
  * @author Jack Meng
  * @since 3.1
