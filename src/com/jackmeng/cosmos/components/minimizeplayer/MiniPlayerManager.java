4c4b6c629269cedfe332691220b914c986690720
Jack Meng <jackmeng0814@gmail.com>
2022-07-18 14:43:17 -0400

    refresh gitignore

--- src/com/jackmeng/cosmos/components/minimizeplayer/MiniPlayerManager.java
+++
@@ -1,7 +0,0 @@
-package com.jackmeng.cosmos.components.minimizeplayer;
-
-public interface MiniPlayerManager {
-  final int MINI_PLAYER_MIN_WIDTH = 430;
-  final int MINI_PLAYER_MIN_HEIGHT = 200;
-  final int MINI_PLAYER_BAR_HEIGHT = 25;
-}
--- src/com/jackmeng/tailwind/Transcoder.java
+++
@@ -1,9 +0,0 @@
-package com.jackmeng.tailwind;
-
-import java.io.File;
-
-public interface Transcoder {
-  final int MP3_FORMAT = 1, WAV_FORMAT = 2, OGG_FORMAT = 3;
-  void transcode(int inFormat, int outFormat, File inLocale, File outLocale);
-
-}
