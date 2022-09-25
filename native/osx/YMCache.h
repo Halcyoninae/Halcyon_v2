b951fb23ff10090ec39345deecec5b28684bd9bf
Jack Meng <jackmeng0814@gmail.com>
2022-09-25 13:32:26 -0400

    `add` osx support for fast caching

---
+++ native/osx/YMCache.h
@@ -0,0 +1,14 @@
+//  Created by Adam Kaplan on 8/1/15.
+//  Copyright 2015 Yahoo.
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for YMCache.
+FOUNDATION_EXPORT double YMCacheVersionNumber;
+
+//! Project version string for YMCache.
+FOUNDATION_EXPORT const unsigned char YMCacheVersionString[];
+
+#import "YMMemoryCache.h"
+#import "YMCachePersistenceController.h"
+
